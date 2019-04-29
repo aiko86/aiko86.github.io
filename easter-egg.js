0360d7e0f3abd5c998e0ef3c76aef2be119338f8
marcodndesign <47820283+marcodndesign@users.noreply.github.com>
2019-04-29 13:51:42 +0200

    mystery egg

--- easter-egg.js
+++ easter-egg.js
@@ -11,3 +11,20 @@ easterEggBanner.style.display = "none";
 
 }
 
+// function for the easter egg popup
+var bynderLogo = document.querySelector(".homepage-bynder-logo");
+var closeEasterEggButton = document.querySelector(".close-video-button");
+
+// Events
+bynderLogo.addEventListener('click', openModal);
+closeVideoButton.addEventListener('click', closeModal);
+
+// Open
+function openModal() {
+  videoModal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  videoModal.style.display = 'none';
+}
