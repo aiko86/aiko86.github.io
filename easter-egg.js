9d153347a40710a494c6b4f27837f1942b2865a4
marcodndesign <47820283+marcodndesign@users.noreply.github.com>
2019-05-13 11:51:23 +0200

    banner eastr egg fixed

--- easter-egg.js
+++ easter-egg.js
@@ -11,20 +11,3 @@ easterEggBanner.style.display = "none";
 
 }
 
-// function for the easter egg popup
-var bynderLogo = document.querySelector(".homepage-bynder-logo");
-var closeEasterEggButton = document.querySelector(".close-video-button");
-
-// Events
-bynderLogo.addEventListener('click', openModal);
-closeVideoButton.addEventListener('click', closeModal);
-
-// Open
-function openModal() {
-  videoModal.style.display = 'block';
-}
-
-// Close
-function closeModal() {
-  videoModal.style.display = 'none';
-}
