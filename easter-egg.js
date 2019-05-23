699458da2c3df02d5d51c204a64347a72fe5903d
marcodndesign <47820283+marcodndesign@users.noreply.github.com>
2019-05-23 11:56:46 +0200

    pop up fixed

--- easter-egg.js
+++ easter-egg.js
@@ -1,13 +1,36 @@
+
+ /*
 var easterEggBanner = document.querySelector(".easter-egg-banner");
 var easterEggClose = document.querySelector(".easter-egg-close");
 
 
 //function to close the easter egg fixed banner
 
-easterEggClose.addEventListener('click', toggleBanner);
+window.addEventListener("load", start);
+
+function start(){
+    easterEggClose.addEventListener('click', toggleBanner);
+
+}
 
 function toggleBanner(){
 easterEggBanner.style.display = "none";
 
 }
 
+*/
+
+jQuery(document).ready(function( $ ) {
+
+  /* Close Popup and set session variable */
+	$('.easter-egg-close').click( function(){
+		$('.easter-egg-banner').fadeOut(300);
+		sessionStorage.setItem('closedOnce', '1');
+	});
+	
+  /* Hide Popup if They've already closed it once */
+	if (sessionStorage.getItem('closedOnce') === "1")  {
+       $('.easter-egg-banner').hide(); 
+    }
+ 
+});
